094c59bdfa8ff84872dfd350e1bc320b7ffa22cd
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2019-06-27 10:14:33 +0300

    Removed factory properties

--- eventuate-messaging-kafka-common-micronaut/src/main/java/io/eventuate/messaging/kafka/common/micronaut/EventuateKafkaPropertiesFactory.java
+++ eventuate-messaging-kafka-common-micronaut/src/main/java/io/eventuate/messaging/kafka/common/micronaut/EventuateKafkaPropertiesFactory.java
@@ -7,7 +7,7 @@ import io.micronaut.context.annotation.Requires;
 import io.micronaut.context.annotation.Value;
 
 @Factory
-@Requires(property = "micronaut.eventuate.kafka.properties.factory", value = "true")
+//@Requires(property = "micronaut.eventuate.kafka.properties.factory", value = "true")
 public class EventuateKafkaPropertiesFactory {
 
   @Bean
--- eventuate-messaging-kafka-consumer-micronaut/src/main/java/io/eventuate/messaging/kafka/consumer/micronaut/MessageConsumerKafkaFactory.java
+++ eventuate-messaging-kafka-consumer-micronaut/src/main/java/io/eventuate/messaging/kafka/consumer/micronaut/MessageConsumerKafkaFactory.java
@@ -3,11 +3,13 @@ package io.eventuate.messaging.kafka.consumer.micronaut;
 import io.eventuate.messaging.kafka.basic.consumer.EventuateKafkaConsumerConfigurationProperties;
 import io.eventuate.messaging.kafka.common.EventuateKafkaConfigurationProperties;
 import io.eventuate.messaging.kafka.consumer.MessageConsumerKafkaImpl;
+import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Requires;
 
 import javax.inject.Singleton;
 
-@Requires(property = "micronaut.eventuate.message.consumer.kafka.factory", value = "true")
+//@Requires(property = "micronaut.eventuate.message.consumer.kafka.factory", value = "true")
+@Factory
 public class MessageConsumerKafkaFactory {
   @Singleton
   public MessageConsumerKafkaImpl messageConsumerKafka(EventuateKafkaConfigurationProperties props,
