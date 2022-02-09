0d4af2d3cef6fb86f92983028a65682afbeec137
Chris Richardson <chris@chrisrichardson.net>
2022-02-08 19:19:53 -0800

    Cleanup workflow

--- build-and-test-all-locally.sh
+++ build-and-test-all-locally.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#! /bin/bash -e
 
 ./kafka/build-docker-kafka-multi-arch.sh
 
-docker pull ${KAFKA_MULTI_ARCH_IMAGE:-${DOCKER_HOST_NAME:-host.docker.internal}:5002/eventuate-kafka:multi-arch-local-build}
+docker pull localhost:5002/eventuate-kafka:multi-arch-local-build
 
 ./build-and-test-all.sh
