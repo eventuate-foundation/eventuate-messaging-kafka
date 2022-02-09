6596a6dec3aa1e0126f77af70b5f71b95bcb8200
Chris Richardson <chris@chrisrichardson.net>
2022-02-08 16:17:38 -0800

    Added echos to debug

--- kafka/build-docker-kafka-multi-arch.sh
+++ kafka/build-docker-kafka-multi-arch.sh
@@ -4,8 +4,14 @@ SCRIPT_DIR=$(cd $( dirname "${BASH_SOURCE[0]}" ) ; pwd)
 
 docker-compose -f $SCRIPT_DIR/../docker-compose-registry.yml --project-name eventuate-common-registry up -d registry
 
+IMAGE="${KAFKA_MULTI_ARCH_IMAGE:-${DOCKER_HOST_NAME:-host.docker.internal}:5002/eventuate-kafka:multi-arch-local-build}"
+OPTS="${BUILDX_PUSH_OPTIONS:---output=type=image,push=true,registry.insecure=true}"
+
+echo IMAGE=$IMAGE
+echo OPTS=$OPTS
+
 docker buildx build --platform linux/amd64,linux/arm64 \
-  -t ${KAFKA_MULTI_ARCH_IMAGE:-${DOCKER_HOST_NAME:-host.docker.internal}:5002/eventuate-kafka:multi-arch-local-build} \
+  -t $IMAGE \
   -f $SCRIPT_DIR/Dockerfile \
-  ${BUILDX_PUSH_OPTIONS:---output=type=image,push=true,registry.insecure=true} \
+  $OPTS \
   $SCRIPT_DIR
