3b450f95a0066f4ed3b2b609ccac475c4b03c059
Chris Richardson <chris@chrisrichardson.net>
2023-08-01 15:23:26 -0700

    Support KAFKA_LOG_DIRS env var to handle problems with mounting volumes with lost+found

--- kafka/run-kafka.sh
+++ kafka/run-kafka.sh
@@ -4,6 +4,8 @@ set -e
 
 EFFECTIVE_KAFKA_CONFIG_DIR=./config
 
+export KAFKA_LOG_DIRS=${KAFKA_LOG_DIRS:-/tmp/kafka-logs}
+
 envsubst < /usr/local/kafka-config/server.properties > $EFFECTIVE_KAFKA_CONFIG_DIR/server.properties
 
 exec bin/kafka-server-start.sh $EFFECTIVE_KAFKA_CONFIG_DIR/server.properties
